baef5fecdbdd2fb14b4599ab75c5359448270f47
PrabodhaNirmani <prabodhanh@gmail.com>
2017-05-15 21:09:44 +0530

    change server url

--- src/providers/app-settings.ts
+++ src/providers/app-settings.ts
@@ -3,7 +3,8 @@ import { Http } from "@angular/http";
 
 
 const CONFIG={
-  apiUrl:"http://127.0.0.1:5000/"
+  // apiUrl:"http://127.0.0.1:5000/"
+  apiUrl:"https://blood-donor-app-server.herokuapp.com/"
 }
 const districts=[{d:'Matara'},{d:'Galle'},{d:'Hambanthota'},{d:'Colombo'},{d:'Gampaha'},{d:'Kaluthara'},{d:'Monaragala'},{d:'Badulla'},{d:'Kandy'},{d:'Mathale'},{d:'Nuwara Eliya'},{d:'Ampara'},{d:'Batticaloa'},{d:'Trincomalee'},{d:'Anuradhapura'},{d:'Polonnaruwa'},{d:'Kegalle'},{d:'Rathnapura'},{d:'Kurunegalle'},{d:'Puttalam'},{d:'Jaffna'},{d:'Vavniya'},{d:'Mullaitivu'},{d:'Mannar'},{d:'Kilinochchi'}];
  
